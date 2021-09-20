eca2d8f0dacd169a6953cabb428778a2e14e44e9
root <root@VM.zf3pka43i2huplldqisiut4fpb.parx.internal.cloudapp.net>
2021-09-20 12:29:38 +0000

    fix init.sh

--- init.sh
+++ init.sh
@@ -27,16 +27,16 @@
     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
-    # 5. Install Java JRE
+    # # 5. Install Java JRE
 
-    sudo apt-get install openjdk-8-jre
+    # sudo apt-get install openjdk-8-jre
 
-    # 6. Install Jmeter
+    # # 6. Install Jmeter
 
-    mkdir jmeter
-    cd jmeter
-    wget http://www.gtlib.gatech.edu/pub/apache/jmeter/binaries/apache-jmeter-5.4.tgz
-    tar -xf apache-jmeter-5.4.tgz
+    # mkdir jmeter
+    # cd jmeter
+    # wget http://www.gtlib.gatech.edu/pub/apache/jmeter/binaries/apache-jmeter-5.4.tgz
+    # tar -xf apache-jmeter-5.4.tgz
 
     # 7. Install cypress dependencies
     sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
@@ -44,3 +44,6 @@
     # 8. Install python module
     sudo apt install python3-pip
     pip install openpyxl
+        # 9. Build img
+    sudo docker-compose build
+ 
