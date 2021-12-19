a1986ca5f9d9e08092bb3a4e9dd29729a744a324
Ubuntu <ubuntu@ip-172-31-6-192.eu-central-1.compute.internal>
2021-12-19 10:52:24 +0000

    change build app sc

--- TestApp/BuildApp.sh
+++ TestApp/BuildApp.sh
@@ -14,17 +14,20 @@ function build(){
     python3 $APP_SERVER_FILE_PATH/CreateEnv.py  $idTest $idMod a
     sleep 1
     env="$(cat $APP_SERVER_FILE_PATH.env)"
-    echo $env
-    bashCommand='cd '$APP_PATH'/'$APP_DIR';
 
-    sudo docker-compose down
-    sudo rm .env
+    
+    bashCommand='cd ~/'$APP_PATH'/'$APP_DIR';
+    sudo docker-compose down;
+    sleep 2;
+    sudo rm .env;
+    sleep 2;
     echo "'$env'" > .env;
-    sleep 3;
-    sudo docker-compose build --no-cache
-    sleep 3;
-    sudo docker-compose up -d
+    sleep 12;
+    sudo docker-compose build --no-cache;
+    sleep 15;
+    sudo docker-compose up -d;
     '
+echo $bashCommand
 
     . RemoteServer.sh $KEY $REMOTE_HOST $USER_SYSTEM "$bashCommand"
     sleep 160
@@ -39,11 +42,14 @@ function buildDataGeneration(){
     GEN_DIR="$(python3 operationOnConfig.py -getAppGenDataDir $idTest -mod $idMod 2>&1)"
 
     bashCommand='cd '$APP_PATH'/'$GEN_DIR';
-    sudo rm .env
-    sudo docker-compose down
+    sudo docker-compose down;
+    sudo rm .env;
+    sleep 2;
     echo "'$env'" > .env;
-    sudo docker-compose build --no-cache
-    sudo docker-compose up -d
+    sleep 12;
+    sudo docker-compose build --no-cache;
+    sleep 12;
+    sudo docker-compose up -d;
     '
     echo $bashCommand
     . RemoteServer.sh $KEY $REMOTE_HOST $USER_SYSTEM "$bashCommand"
